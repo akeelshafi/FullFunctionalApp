51ed6a51e399b8b038b9e580be2bba7cade937f3
Akeel shafi <akeelshafi20@gmail.com>
2025-08-07 15:01:50 +0530

    5th commit

--- app/src/main/java/com/LoginInActivity.kt
+++ app/src/main/java/com/LoginInActivity.kt
@@ -1,13 +1,19 @@
 package com
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.akeel.fullfunctionalapp.R
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class LoginInActivity : AppCompatActivity() {
+    lateinit var database: DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,6 +22,39 @@ class LoginInActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val signInBtn = findViewById<Button>(R.id.signInBtn)
+        val nameEdtTxt = findViewById<EditText>(R.id.nameEdtTxt)
+        val emailEdtTxt = findViewById<EditText>(R.id.emailEdtTxt)
+        val passwordEdtTxt = findViewById<EditText>(R.id.passwordEdtTxt)
+
+        signInBtn.setOnClickListener {
+            val name = nameEdtTxt.text.toString()
+            val email = emailEdtTxt.text.toString()
+            val password = passwordEdtTxt.text.toString()
+
+            val user = User(name, email, password)
+            database = FirebaseDatabase.getInstance().getReference("Users")
+            database.child(name).setValue(user)
+                .addOnSuccessListener {
+                    nameEdtTxt.text.clear()
+                    emailEdtTxt.text.clear()
+                    passwordEdtTxt.text.clear()
+
+                    Toast.makeText(this, "User Registered Successfully", Toast.LENGTH_SHORT).show()
+
+                }
+                .addOnFailureListener {
+                    Toast.makeText(this, "Failed to Register", Toast.LENGTH_SHORT).show()
+
+
+                }
+
+
+
+
+
         }
     }
 }
---
+++ app/src/main/java/com/User.kt
@@ -0,0 +1,3 @@
+package com
+
+data class User(val name: String? = null, val email: String? = null, val password: String? = null)
